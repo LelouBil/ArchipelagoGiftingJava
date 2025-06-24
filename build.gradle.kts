1ddb959c377db42c2c017c730650406737c69533
LelouBil <bilel.medimegh@gmail.com>
2025-06-24 04:02:01 -0400

    Added release automation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,13 @@
+import org.jreleaser.model.Active
 import java.net.URI
 
 plugins {
     alias(libs.plugins.kotlin)
     `maven-publish`
+    alias(libs.plugins.jreleaser)
 }
 
-group = "net.leloubil.archipelago"
+group = "net.leloubil"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -34,14 +36,75 @@ publishing {
     publications {
         create<MavenPublication>("maven") {
             groupId = project.group.toString()
-            artifactId = "gifting-jvm"
-            version = project.version.toString()
+            artifactId = "ArchipelagoGiftingJvm"
 
             from(components["kotlin"])
             artifact(sourcesJar)
+
+            pom {
+                name = "ArchipelagoGiftingJvm"
+                description = "Archipelago Gifting API for JVM"
+                url = "https://github.com/LelouBil/ArchipelagoGiftingJvm"
+                licenses {
+                    license {
+                        name = "MIT"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "LelouBil"
+                    }
+                }
+                scm {
+                    url = "http://github.com/LelouBil/ArchipelagoGiftingJvm"
+                }
+            }
         }
     }
 }
+
+jreleaser {
+    project {
+        authors = listOf("LelouBil")
+        license = "MIT"
+    }
+
+    release {
+        github {
+            repoOwner = "LelouBil"
+            overwrite = true
+        }
+    }
+
+    signing {
+        active = Active.ALWAYS
+        armored = true
+    }
+    deploy {
+        maven {
+            mavenCentral {
+                create("sonatype") {
+                    active = Active.RELEASE
+                    url = "https://central.sonatype.com/api/v1/publisher"
+                    stagingRepository("target/staging-deploy")
+                }
+            }
+            nexus2 {
+                create("snapshot-deploy") {
+                    active = Active.SNAPSHOT
+                    snapshotUrl = "{central_snapshot_url}"
+                    applyMavenCentralRules = true
+                    snapshotSupported = true
+                    closeRepository = true
+                    releaseRepository = true
+                    stagingRepository("target/staging-deploy")
+                }
+            }
+        }
+    }
+
+}
+
 
 tasks.test {
     useJUnitPlatform()
